ac96124055031c17b59277fef8dd990bdb27919b
core.pager=cat <benzaouikeltoum724@gmail.com>
2022-10-04 12:36:47 +0100

    form

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import Formulaire from './Formulaire';
+
+
+function App() {
+  return (
+    <div className="App">
+
+
+<Formulaire></Formulaire>
+
+
+     </div>
+  );
+}
+
+export default App;
